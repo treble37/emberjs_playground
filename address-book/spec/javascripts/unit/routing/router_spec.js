87add08f292d2673af2176f4a1296248b0a3b380
Bruce Park <bruce@binarywebpark.com>
2014-05-29 19:54:58 -0700

    Routing spec to green

--- address-book/spec/javascripts/unit/routing/router_spec.js
+++ address-book/spec/javascripts/unit/routing/router_spec.js
@@ -2,6 +2,7 @@ test('root route', function () {
   visit('/');
   andThen(function () {
     var current_route = AddressBook.__container__.lookup('controller:application').currentRouteName;
-    equal(current_route, '', 'Expected ****** got: ' + current_route);
+    equal(current_route, 'index', 'Expected ****** got: ' + current_route);
   });
+
 })

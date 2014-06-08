5b9b966cd6ac66771db53d0ad73ccde1790ab8f9
Bruce Park <bruce@binarywebpark.com>
2014-06-08 16:14:22 -0700

    connect front end to backend

--- address-book/app/assets/javascripts/store.js
+++ address-book/app/assets/javascripts/store.js
@@ -1,9 +1,9 @@
 AddressBook.Store = DS.Store.extend({
-
+  adapter: '-active-model'
 });
 
 // Override the default adapter with the `DS.ActiveModelAdapter` which
 // is built to work nicely with the ActiveModel::Serializers gem.
 AddressBook.ApplicationAdapter = DS.ActiveModelAdapter.extend({
-
+  {namespace: "api/v1"}
 });
